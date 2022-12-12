cb4d73fd1d595de0dd223da2b6c80b260ed8e2f0
chad hinds <chadrickj8@gmail.com>
2022-12-12 12:12:11 -0500

    Add spinner logic

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 // Sets up env access
 const dotenv = require('dotenv').config();
@@ -5,8 +6,12 @@ const port = process.env.PORT || 5050;
 
 const app = express();
 
+// Enable body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/openai', require('./routes/openaiRoutes'))
 
